0628c269748fe15aa2d933be4cb1c045cdad0c63
Wesley_Johnson <johnsonwesley167@gmail.com>
2020-09-28 23:22:31 +0530

    015 - Reset Game session.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -11,9 +11,11 @@ public class SceneLoader : MonoBehaviour {
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
 
+
     public void LoadStartScene()
     {
         SceneManager.LoadScene(0);
+        FindObjectOfType<GameSession>().ResetGame();
     }
 
     public void QuitGame()
